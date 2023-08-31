9601f36a4cbcd701e92206937f22a26491dbef4f
DivyamShma <divyamsharma511@gmail.com>
2023-08-31 16:43:03 +0530

    link submission addedd

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from .models import links
 
 class MyForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
+
+class fetch(forms.Form):
+    link = forms.CharField(max_length=100)
+    class Meta:
+        model = links
